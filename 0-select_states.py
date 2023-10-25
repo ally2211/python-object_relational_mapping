7afc71b3e72c1f6f020151138b755dbdfd56b288
ally2211 <amendieta.va@gmail.com>
2023-10-25 13:02:24 -0700

    redo

--- 0-select_states.py
+++ 0-select_states.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""
-module documentation:  lets connect to MySqldb
-"""
+"""Write a script that lists all states from the database hbtn_0e_0_usa"""
+import sys
 import MySQLdb
 
 
-conn = MySQLdb.connect(host="localhost", port=3306, user="root",
-                       passwd="root", db="hbtn_0e_0_usa", charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
